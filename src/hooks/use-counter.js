73a496f4eb865276862957ddf970b843b86d31c9
Shilpa <mulgeshilpa@gmail.com>
2023-02-23 13:01:30 +0530

    added useCounter custom hook

---
+++ src/hooks/use-counter.js
@@ -0,0 +1,18 @@
+const useCounter = (forword = true) => {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (forword) {
+                setCounter((prevCounter) => prevCounter + 1);
+            } else {
+                setCounter((prevCounter) => prevCounter - 1);
+            }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return counter;
+}
+export default useCounter;
